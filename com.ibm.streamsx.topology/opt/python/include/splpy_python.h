56b98bc1f30dfaf232ca97de793934b5778ceae2
Dan Debrunner <djd@debrunners.com>
2019-10-08 20:26:46 +0000

    add temp workarounds

--- com.ibm.streamsx.topology/opt/python/include/splpy_python.h
+++ com.ibm.streamsx.topology/opt/python/include/splpy_python.h
@@ -18,13 +18,20 @@
 #ifndef __SPL__SPLPY_PYTHON_H
 #define __SPL__SPLPY_PYTHON_H
 
-#define Py_LIMITED_API ((3 << 24) | (5 << 16))
+// TEMP until all macros are defined
+#define XXXXX_Py_LIMITED_API ((3 << 24) | (5 << 16))
+
+#include "Python.h"
+
+// TEMP to ensure macros are correct while
+// not all missing macros have been added.
+#undef PyTuple_GET_SIZE
+#undef PyTuple_GET_ITEM
+#undef PyTuple_SET_ITEM
 
 #define PyTuple_GET_SIZE(t) PyTuple_Size(t)
 #define PyTuple_GET_ITEM(t, pos) PyTuple_GetItem((t), (pos))
 #define PyTuple_SET_ITEM(t, pos, o) PyTuple_SetItem((t), (pos), (o))
-
-#include "Python.h"
 
 #endif
 
